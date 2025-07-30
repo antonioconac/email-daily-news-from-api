5b89868f87fd9d4f19fbf3fcaf5424fe35b3f9b9
antonioconac <conacheantonio13@gmail.com>
2025-07-30 13:16:00 +0300

    Made the password private to the public

--- send_email.py
+++ send_email.py
@@ -5,7 +5,7 @@ def send_email(message):
     port = 465
 
     username = "conacheantonio13@gmail.com"
-    password = "xylbonnlcnpzxpfp"
+    password = "YOUR_GMAIL_APP_PASSWORD"
 
     receiver = "conacheantonio13@gmail.com"
     context = ssl.create_default_context()
